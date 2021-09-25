69cbb35ca24bbecc593e3dc4b05533a9878ae894
juanupc <91358745+juanupc@users.noreply.github.com>
2021-09-24 23:56:22 -0500

    calculadora
    mod #1

--- calculadora/calculadora.java
+++ calculadora/calculadora.java
@@ -20,19 +20,19 @@ public class calculadora {
 			System.out.println(" 4/ Division ");
 			System.out.println(" 5/ Potencia");
 			System.out.println(" 6/ Raiz cuadrada");
-			System.out.println(" 7/ Seno");
-			System.out.println(" 8/ Coseno");
-			System.out.println(" 9/ Tangente");
-			System.out.println(" 10/ Arcoseno");
-			System.out.println(" 11/ Arcocoseno");
-			System.out.println(" 12/ Arcotangente");
-			System.out.println(" 13/ Logaritmo natural");
-			System.out.println(" 14/ Convertir grados a radiandes");
-			System.out.println(" 15/ Convertir radianes a grados");
-			System.out.println(" 16/ Redondear numeros");
-                        System.out.println(" 17/ factorial");
-			System.out.println(" 18/ valor absoluto");
-                        System.out.println(" 19/ Raiz cubica");
+			System.out.println(" 7/ Raiz cubica");
+			System.out.println(" 8/ Seno");
+			System.out.println(" 9/ Coseno");
+			System.out.println(" 10/ Tangente");
+			System.out.println(" 11/ Arcoseno");
+			System.out.println(" 12/ Arcocoseno");
+			System.out.println(" 13/ Arcotangente");
+			System.out.println(" 14/ Logaritmo natural");
+			System.out.println(" 15/ Convertir grados a radiandes");
+			System.out.println(" 16/ Convertir radianes a grados");
+			System.out.println(" 17/ Redondear numeros");
+                        System.out.println(" 18/ factorial");
+			System.out.println(" 19/ valor absoluto");
 			System.out.println(" 20/ Cerrar ");
 			System.out.print(" Seleccione una de la anteriores opciones ");
 			op = Integer.parseInt(bufEntrada.readLine());
@@ -85,34 +85,39 @@ public class calculadora {
 			case 7:
 				System.out.print(" Ingrese el numero ");
 				num_1 = Double.parseDouble(bufEntrada.readLine());
+				System.out.println(" La raiz cubica "+num_1+" Es "+Math.cbrt(num_1));
+				break;
+					
+			case 8:	System.out.print(" Ingrese el numero ");
+				num_1 = Double.parseDouble(bufEntrada.readLine());
 				System.out.println(" El seno de "+num_1+" Es "+Math.sin(num_1));
 				break;
-			case 8:
+			case 9:
 				System.out.print(" Ingrese el numero ");
 				num_2 = Double.parseDouble(bufEntrada.readLine());
 				System.out.println(" El coseno de "+num_2+" Es "+Math.cos(num_2));
 				break;
-			case 9:
+			case 10:
 				System.out.print(" Ingrese el valor se seno ");
 				num_1 = Double.parseDouble(bufEntrada.readLine());
 				System.out.println(" La tangente de "+num_1+" Es "+Math.tan(num_1));
 				break;
-			case 10:
+			case 11:
 				System.out.print(" Ingrese el numero ");
 				num_1 = Double.parseDouble(bufEntrada.readLine());
 				System.out.println(" El arcoseno de "+num_1+" Es "+Math.asin(num_1));
 				break;
-			case 11:
+			case 12:
 				System.out.print(" Ingrese el numero ");
 				num_2 = Double.parseDouble(bufEntrada.readLine());
 				System.out.println(" El arcocoseno de "+num_2+" Es "+Math.acos(num_2));
 				break;
-			case 12:
+			case 13:
 				System.out.print(" Ingrese el valor se seno ");
 				num_1 = Double.parseDouble(bufEntrada.readLine());
 				System.out.println(" La arcotangente de "+num_1+" Es "+Math.atan(num_1));
 				break;
-			case 13:
+			case 14:
 				System.out.println("Ingresar el numero ");
 				num_1 = Double.parseDouble(bufEntrada.readLine());
 				if (num_1>=0) {
@@ -121,24 +126,24 @@ public class calculadora {
 					System.out.println(" El numero debe ser mayor a cero ");
 				}
 				break;
-			case 14:
+			case 15:
 				System.out.println("Ingrese los grados ");
 				grados = Double.parseDouble(bufEntrada.readLine());
 				rad = grados*(Math.PI/180);
 				System.out.println("Los grados "+grados+" en radianes son "+rad);
 				break;
-			case 15:
+			case 16:
 				System.out.println("Ingrese los radianes");
 				rad = Double.parseDouble(bufEntrada.readLine());
 				grados = rad*(180/Math.PI);
 				System.out.println("Los radianes "+rad+" en grados son "+grados);
 				break;
-			case 16:
+			case 17:
 				System.out.println("Ingrese el valor");
 				num_1 = Double.parseDouble(bufEntrada.readLine());
 				System.out.println("El numero aproximado es "+Math.round(num_1));
 				break;
-                        case 17:
+                        case 18:
                                 System.out.println("Ingrese el valor");
                                 num_1 = Double.parseDouble(bufEntrada.readLine());
                                 for (int i = (int) num_1; i > 0; i--) {
@@ -146,16 +151,12 @@ public class calculadora {
                                 }
                                 System.out.println("El factorial de " + num_1 + " es: " + factorial);
                                 break;
-			case 18:
+			case 19:
 				System.out.println("Ingrese el numero ");
 				num_2 = Double.parseDouble(bufEntrada.readLine());
 				System.out.println("El valor absoluto es "+Math.abs(num_2));
 				break;
-                        case 19:
-				System.out.print(" Ingrese el numero ");
-				num_1 = Double.parseDouble(bufEntrada.readLine());
-				System.out.println(" La raiz cuadrada "+num_1+" Es "+Math.cbrt(num_1));
-				break;
+                     
 			case 20:
 				System.out.println("Saliendo....");
 				System.out.println(" Gracias por utilizar la calculadora ");
